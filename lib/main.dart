3b29b890d14a848aea8e8336a27f0a1841f84803
MARUF HASSAN <MRFHASSAN17@GMAIL.COM>
2022-05-26 19:48:38 +0530

    Added const

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:dribbox_ui/screens/home_screen.dart';
-import 'package:dribbox_ui/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'constants/theme.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(375, 812),
+      designSize: const Size(375, 812),
       builder: () => MaterialApp(
         theme: AppTheme.appTheme,
-        home: HomeScreen(),
+        home: const HomeScreen(),
       ),
     );
   }
